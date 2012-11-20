7195efb0b5fcebb7bc4bf21f3046d4fdfbd01666
pmy <pmy@google.com>
2012-11-20 07:30:54 +0000

    First cut at angular 1+ demo.

--- war/index.js
+++ war/index.js
@@ -1,27 +1,16 @@
-angular.service('Items', function($resource) {
-    return $resource('/data/numbers');
-  });
+'use strict';
 
-function ItemRsrc(Items) {
-  this.items = Items.query();
-  this.add = function() {
-    var obj = new Items();
-    obj['a'] = 'foo';
-    obj.$save();
-    this.items.push(obj);
-  }
+angular.module('demo', ['datasetService']);
+
+angular.module('datasetService', ['ngResource']).
+  factory('Dataset', function($resource){
+      return $resource('/data/:name', {}, {});
+    });
+
+function DatasetCtrl($scope, $routeParams, Dataset) {
+  // $routeParams.name matches "when('/:name'," above.
+  $scope.dataset = Dataset.get({name: $routeParams.name}, function(f) {
+    });
 }
 
-angular.widget('my:input', function(expression, compileElement) {
-    var compiler = this;
-    var expr = compileElement.attr('expr');
-    console.log(expr);
-    return function(linkElement) {
-      var currentScope = this;
-      currentScope.$watch(expr, function(value) {
-          for (propName in value)
-            if (propName.substring(0,1) != '$') // ignore angular extensions
-              console.log(propName + ':' + value[propName]);
-        });
-    };
-  });
+DatasetCtrl.$inject = ['$scope', '$routeParams', 'Dataset'];
